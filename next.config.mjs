0a194ec9107e76ce510cc54e67faaacc34d1bf00
sooki88 <sookhee9n@gmail.com>
2024-05-13 10:32:18 +0900

    Chore: next.config.mjs에 swcMinify와 compiler 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
   images: {
     remotePatterns: [
       {
